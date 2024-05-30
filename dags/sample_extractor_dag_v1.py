b5cb5c370f6102c8280ca9bb0a5f82992db8b8ee
Dexter <dexter.lente18@gmail.com>
2024-05-30 18:09:46 +0800

    added sample

---
+++ dags/sample_extractor_dag_v1.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from samples.sample_extractor_py import task_one, task_two
+
+default_args = {
+    'owner': 'admin',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=2)
+}
+
+
+dag = DAG(
+    dag_id='sample_extractor_dag_v1',
+    default_args=default_args,
+    description='This is first simple extractor dag',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval='@daily'
+)
+
+first_task = PythonOperator(
+    task_id='first_task',
+    python_callable=task_one,
+    dag=dag,
+)
+
+
+second_task = PythonOperator(
+    task_id='second_task',
+    python_callable=task_two,
+    dag=dag,
+)
---
+++ dags/samples/sample_extractor_py.py
@@ -0,0 +1,6 @@
+def task_one():
+    print('task one is printed')
+
+
+def task_two():
+    print('print this second task')
